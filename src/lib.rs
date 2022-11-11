6f03ecada827592a7730c30948fe9314cecaca5e
Max Dymond <cmeister2@gmail.com>
2022-11-11 22:54:16 +0000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,116 @@
+//! Copies the currently running program into a temporary location
+//!
+//! This crate copies the currently running executable into a
+//! temporary location and returns the path to that executable.
+//! This allows you to (for example):
+//!
+//! - Compile a program statically using something like musl
+//! - Create a copy of that program while it's running
+//! - Run Docker from your original program, mounting the copy as a Docker volume mount
+//! - Run the copied program from within the Dockerized environment.
+//!
+//! Because this library uses [`NamedTempFile`] to generate a temporary location,
+//! the following security restrictions apply to [`Replicate`]:
+//!
+//! 1. The copy has a short lifetime and your temporary file cleaner is sane (doesn’t delete recently accessed files).
+//! 2. You trust every user on your system (i.e. you are the only user).
+//! 3. You have disabled your system’s temporary file cleaner or verified that your system doesn’t have a temporary file cleaner.
+//!
+#![deny(
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results
+)]
+
+#[cfg(unix)]
+use std::os::unix::fs::PermissionsExt;
+use std::{fs::Permissions, ops::Deref, path::Path};
+
+use palaver::env::exe;
+use tempfile::{Builder, TempPath};
+use thiserror::Error;
+
+/// Possible errors while creating a replicate of the running program
+#[derive(Debug, Error)]
+pub enum ReplicateError {
+    /// A file operation failed while creating the replicate; check
+    /// the associated error for more information.
+    #[error("File operation failed while creating replicate")]
+    IOError(#[from] std::io::Error),
+}
+
+/// A temporary copy of the running executable.
+pub struct Replicate {
+    path: TempPath,
+}
+
+impl Replicate {
+    /// Creates a replicate of the currently running program. The
+    /// copy is deleted when this is dropped.
+    pub fn new() -> Result<Self, ReplicateError> {
+        // Use palaver to get a `File` reference to the currently running program.
+        let mut self_exe = exe()?;
+
+        // Create a new temporary file.
+        let mut copy = Builder::new()
+            .prefix("replicate_")
+            .rand_bytes(5)
+            .tempfile()?;
+
+        // Copy the contents of this program into the copy.
+        let _ = std::io::copy(&mut self_exe, &mut copy)?;
+
+        // Convert the copy into a TempPath so we can pass around the path info.
+        let path = copy.into_temp_path();
+
+        // If necessary make the copy executable.
+        #[cfg(unix)]
+        {
+            let permissions = Permissions::from_mode(0o755);
+            std::fs::set_permissions(&path, permissions)?;
+        }
+
+        // Return the Replicate.
+        Ok(Self { path })
+    }
+}
+
+impl Deref for Replicate {
+    type Target = Path;
+
+    fn deref(&self) -> &Path {
+        &self.path
+    }
+}
+
+impl AsRef<Path> for Replicate {
+    fn as_ref(&self) -> &Path {
+        &self.path
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::OsStr;
+
+    use super::*;
+
+    #[test]
+    fn create_replicate() -> anyhow::Result<()> {
+        let copy = Replicate::new()?;
+        println!("Created new copy: {}", copy.display());
+
+        let name = copy
+            .file_name()
+            .and_then(OsStr::to_str)
+            .expect("Failed to copy program");
+
+        // Verify the name starts with "replicate"
+        assert!(name.starts_with("replicate"));
+        Ok(())
+    }
+}
